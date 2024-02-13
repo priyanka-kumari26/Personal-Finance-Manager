0a9125c0bb3e3f542cee5b2a9835af4a3b73c669
priyanka-kumari26 <swetapriya2612@gmail.com>
2024-02-13 15:53:52 +0530

    Adding all files

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const express = require ('express');
+const cors = require ('cors');
+const {db} =require ('./db/db')
+const {readdirSync}= require ('fs')
+
+const app = express()
+require ('dotenv').config()
+
+
+const PORT =process.env.PORT 
+
+app.use(express.json())
+app.use(cors())
+ 
+//Reads the contents of the './routes' directory synchronously
+
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+
+const server =() =>{
+   db()
+   app.listen (PORT, ()=>{
+    console.log('Listening to port:',PORT);
+   })
+}
+
+server()
